7fe7335a89fec07dd94a58c37ad512101c5ea273
nitin-jadhavcloud <nkjadhav0606@gmail.com>
2025-07-18 20:52:25 +0530

    Delete .TF Files directory

--- .TF Files/S3.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket" "mybucket" {
-  bucket = var.s3_bucket_name
-  acl    = "private"
-  tags = {
-    Name = "DevOps Bucket"
-  }
-}
--- .TF Files/iam.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_iam_user" "readonly_user" {
-  name = "readonly-user"
-}
-
-resource "aws_iam_policy_attachment" "attach_readonly" {
-  name       = "readonly-attach"
-  users      = [aws_iam_user.readonly_user.name]
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
-}
